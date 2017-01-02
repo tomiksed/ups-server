bc4a91300fe19370e195b808cd945c356ca54a54
Tomas Sedivy <sedivyt8@gmail.com>
2017-01-02 20:09:14 +0100

    A

--- UPSServer/main.cpp
+++ UPSServer/main.cpp
@@ -1,16 +1,97 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
 #include "network/PacketManager.h"
 #include "network/Server.h"
 #include "network/Sender.h"
-#include "datatypes/Message.h"
-#include "helpers/Serializer.h"
+#include "helpers/MainOptions.h"
 #include "network/CommandManager.h"
 
 
 #include <cstdint>
+#include <stdlib.h>
+
+void printHelp() {
+    std::cout << "Usage: traxServer [options]" << std::endl
+              << "\tPossible options:" << std::endl
+              << "\t\t-p <port number>     - port to listen on" << std::endl
+              << "\t\t-l <log level>     - log level (0 - 3)" << std::endl;
+}
+
+bool isNumeric(std::string str) {
+    for (char c : str) {
+        if (c < '0' || c > '9') {
+            return false;
+        }
+    }
+    return true;
+}
+
+int loglevel = 1;
+int port = 55555;
+
+void doParameters(int argc, char *argv[]) {
+    MainOptions mo(argc, argv);
+
+    if (mo.hasKey("-h")) {
+        printHelp();
+        _exit(0);
+    }
+
+    if (mo.hasKey("-l")) {
+        MainOptions::Option* opt = mo.getParamFromKey("-l");
+        std::string type = opt ? (*opt).second : "";
+
+        if (type != "") {
+            if (isNumeric(type) && atoi(type.c_str()) <= 3 && atoi(type.c_str()) >= 0) {
+                loglevel = atoi(type.c_str());
+            } else {
+                std::cerr << "Wrong log level option, using default" << std::endl;
+            }
+        } else {
+            printHelp();
+            _exit(-1);
+        }
+    }
+
+    if (mo.hasKey("-p")) {
+        MainOptions::Option* opt = mo.getParamFromKey("-p");
+        std::string type = opt ? (*opt).second : "";
+
+        if (type != "") {
+            if (isNumeric(type) && atoi(type.c_str()) <= 65535 && atoi(type.c_str()) > 0) {
+                port = atoi(type.c_str());
+            } else {
+                std::cerr << "Wrong port option, using default" << std::endl;
+            }
+        } else {
+            printHelp();
+            _exit(-1);
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
-    Logger::instance()->init(log::DEBUG);
+    doParameters(argc, argv);
+
+    switch (loglevel) {
+        case 0: {
+            Logger::instance()->init(log::DEBUG);
+            break;
+        }
+        case 1: {
+            Logger::instance()->init(log::INFO);
+            break;
+        }
+        case 2: {
+            Logger::instance()->init(log::WARNING);
+            break;
+        }
+        case 3: {
+            Logger::instance()->init(log::ERROR);
+            break;
+        }
+    }
 
     Sender::instance()->init();
     Sender::instance()->start();
@@ -21,66 +102,12 @@ int main(int argc, char *argv[]) {
     CommandManager::instance()->init();
     CommandManager::instance()->start();
 
-    Server::instance()->init(55555);
+    Server::instance()->init(port);
+
+    LOG_INFO("Starting server at port " + std::to_string(port));
     std::thread *serverThread = Server::instance()->start();
 
-     /**/
 
-    /*uint32_t pokus = (0x01 << 24) | (0x02 << 16) | (0x03 << 8) | (0x04);
-    std::cout << "Pokus is: " << pokus << std::endl;
-    if (((uint8_t *) &pokus)[0] == 0x01) {
-        std::cout << "Big endian!" << std::endl;
-    } else {
-        std::cout << "Little endian!" << std::endl;
-    }
-
-    std::cout << "First byte =" << std::to_string(((uint8_t *) &pokus)[0]) << std::endl; /**/
-
-    /*uint32_t pokus = 5;
-    Message *pokusna = new Message(POKUSTON, std::string("siissis"));
-    pokusna->addData((void *) new std::string("Toto je pokuston pokusovaty"));
-    pokusna->addData(&pokus);
-    uint32_t pokusa = 0xa01536;
-    pokusna->addData(&pokusa);
-    pokusna->addData((void *) new std::string("didi;tomik;luisa;pepa;franta"));
-    pokusna->addData((void *) new std::string("Ahoj vole, jak se mas?"));
-    pokusna->addData(&pokus);
-    pokusna->addData((void *) new std::string("Dobre"));
-
-    int resultSize = 0;
-    uint8_t *serZprava = Serializer::instance()->serialize(pokusna, &resultSize);
-
-    Message *deserializovana = Serializer::instance()->deserialize(serZprava);
-
-    uint32_t *intPayload;
-    std::string *stringPayload;
-    const std::vector<void *> *payload = deserializovana->getPayload();
-
-    int i = 0;
-    for (char f : deserializovana->getMsgFormat()) {
-        switch (f) {
-            case 'i': {
-                intPayload = (uint32_t *) payload->at(i);
-
-                LOG_DEBUG("INT payload: " + std::to_string((*intPayload)));
-
-                break;
-            }
-            case 's': {
-                stringPayload = (std::string *) payload->at(i);
-
-                LOG_DEBUG("STRING payload: " + *stringPayload);
-
-                break;
-            }
-        }
-        i++;
-    }/**/
-
-    /*for (int i = 0; i < resultSize; i++) {
-        LOG_DEBUG(std::string("") + std::to_string(serZprava[i]));
-    }
-    /**/
     serverThread->join();
 
     return 0;
